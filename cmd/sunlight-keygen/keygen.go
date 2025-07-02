f62f9084016c4c377d3855471720d7d0cdea3663
Filippo Valsorda <hi@filippo.io>
2025-07-02 19:15:00 +0200

    cmd/sunlight-keygen: actually generate the seed
    Fixes #35

--- cmd/sunlight-keygen/keygen.go
+++ cmd/sunlight-keygen/keygen.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/ed25519"
 	"crypto/elliptic"
+	"crypto/rand"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
@@ -11,24 +12,49 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/url"
 	"os"
 
 	"filippo.io/keygen"
+	"filippo.io/sunlight/internal/immutable"
 	"golang.org/x/crypto/hkdf"
 	"golang.org/x/mod/sumdb/note"
 )
 
 func main() {
 	fs := flag.NewFlagSet("keygen", flag.ExitOnError)
+	fileFlag := fs.String("f", "", "path to the seed file")
+	prefixFlag := fs.String("prefix", "", "submission prefix for the log, to output a witness verifier key")
 	fs.Parse(os.Args[1:])
-
-	if fs.NArg() != 2 {
-		log.Fatal("usage: sunlight-keygen [-pem] <name> <seed file>")
+	if fs.NArg() != 0 || *fileFlag == "" {
+		fmt.Fprintln(os.Stderr, "usage: sunlight-keygen -f <seed file>")
+		fs.PrintDefaults()
+		os.Exit(2)
 	}
 
-	seed, err := os.ReadFile(fs.Arg(1))
+	if _, err := os.Stat(*fileFlag); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "Creating new immutable seed file at: %s\n", *fileFlag)
+		seed := make([]byte, 32)
+		rand.Read(seed)
+		f, err := os.OpenFile(*fileFlag, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o400)
+		if err != nil {
+			log.Fatal("failed to create seed file:", err)
+		}
+		if _, err := f.Write(seed); err != nil {
+			log.Fatal("failed to write seed file:", err)
+		}
+		immutable.Set(f)
+		if err := f.Close(); err != nil {
+			log.Fatal("failed to close seed file:", err)
+		}
+	}
+
+	seed, err := os.ReadFile(*fileFlag)
 	if err != nil {
 		log.Fatal("failed to load seed:", err)
+	}
+	if len(seed) != 32 {
+		log.Fatal("seed file must be exactly 32 bytes")
 	}
 
 	ecdsaSecret := make([]byte, 32)
@@ -57,13 +83,21 @@ func main() {
 
 	edPubKey := string(pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: wk}))
 
-	v, err := note.NewEd25519VerifierKey(fs.Arg(0), wk)
-	if err != nil {
-		log.Fatal("failed to create verifier key:", err)
-	}
-
 	fmt.Printf("Log ID: %s\n", base64.StdEncoding.EncodeToString(logID[:]))
-	fmt.Printf("ECDSA public key:\n%s\n", ecPubKey)
-	fmt.Printf("Ed25519 public key:\n%s\n", edPubKey)
-	fmt.Printf("Witness verifier key: %s\n", v)
+	if *prefixFlag != "" {
+		prefix, err := url.Parse(*prefixFlag)
+		if err != nil {
+			log.Fatal("failed to parse submission prefix:", err)
+		}
+		if prefix.Scheme == "" || prefix.Host == "" {
+			log.Fatal("submission prefix must be a valid URL with scheme and host")
+		}
+		v, err := note.NewEd25519VerifierKey(prefix.Host+prefix.Path, wk)
+		if err != nil {
+			log.Fatal("failed to create verifier key:", err)
+		}
+		fmt.Printf("Verifier key: %s\n", v)
+	}
+	fmt.Printf("ECDSA public key:\n%s", ecPubKey)
+	fmt.Printf("Ed25519 public key:\n%s", edPubKey)
 }
